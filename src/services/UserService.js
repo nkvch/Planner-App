b7846ab9e08d9b8aa5dfbbd06c2772395ed0d337
Stanislau Stankevich <s.stankevich@stud.elka.pw.edu.pl>
2021-04-30 09:11:45 +0200

    change url

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const USERS_REST_API_URL = 'http://localhost:8080/api/userprofile';
+const USERS_REST_API_URL = 'http://localhost:8080/api/user';
 
 class UserService {
 
